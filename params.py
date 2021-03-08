1b1c3d7d6c28ec303be9472a7c4669459070c0c1
proyectoemergeoftalents@gmail.com <proyectoemergeoftalents@gmail.com>
2021-03-07 20:22:55 -0500

    All

--- params.py
+++ params.py
@@ -2,7 +2,7 @@ def conf():
     params = {
     "driver": r'C:\chrome\chromedriver.exe',
     "url": r"https://np-doczilla.liberty.cl/",
-    "username": "mario.ortega@libertymutual.com",
-    "password": "Johnson2012Pasto2012@"
+    "username": "****",
+    "password": "****"
     }
     return params; 
